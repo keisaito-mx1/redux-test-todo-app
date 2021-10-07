b22fe272a3c904e47d43dd69dd79ecd3aad82180
kei saito <keisaito@mx1.wiseman.co.jp>
2021-10-07 20:59:07 +0900

    feat:update

--- src/app/components/templates/AppTemplate.tsx
+++ src/app/components/templates/AppTemplate.tsx
@@ -1,8 +1,14 @@
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
 
 const Wrapper = styled.div({
-  display: 'flex',
-  flexDirection: 'column',
+  margin: "0 auto",
+  width: "100%",
+  maxWidth: "400px",
+  height: "100vh",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  flexDirection: "column",
 });
 
 const AppTemplate: React.FC = ({ children }) => {
--- src/app/components/templates/TodoTemplate.tsx
+++ src/app/components/templates/TodoTemplate.tsx
@@ -1,7 +1,11 @@
-import styled from '@emotion/styled';
-import HeaderForm from '../organisms/HeaderForm';
+import styled from "@emotion/styled";
+import HeaderForm from "../organisms/HeaderForm";
 
-const Wrapper = styled.div({});
+const Wrapper = styled.div({
+  margin: "0 auto",
+  maxWidth: "640px",
+  height: "100vh",
+});
 
 const TodoTemplate: React.FC = ({ children }) => {
   return (
